681349369dcca1ec03c16085815b8f88d7107cd1
nhmikami <nhmikami@gmail.com>
2025-02-24 20:07:11 -0300

    first commit

--- src/main.c
+++ src/main.c
@@ -1,26 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: naharumi <naharumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/21 17:07:06 by naharumi          #+#    #+#             */
-/*   Updated: 2025/02/24 17:57:18 by naharumi         ###   ########.fr       */
+/*   Created: 2025/02/24 15:45:20 by naharumi          #+#    #+#             */
+/*   Updated: 2025/02/24 20:06:11 by naharumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "../inc/minishell.h"
 
-# include <fcntl.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <signal.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <unistd.h>
-# include "../lib/libft.h"
+int main(int ac, char **av, char **envp)
+{
+    char    *input;
 
-#endif
+    (void)ac;
+    (void)av;
+    while (1)
+    {
+        // signals
+        input = readline("minishell$ ");
+        if (!input)
+            break;
+        if (*input)
+            add_history(input);
+        // tokenizer
+        // execute
+        
+        free(input);
+    }
+    return (0);
+}
