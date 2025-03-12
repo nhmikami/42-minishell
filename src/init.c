75e1e86b21e9b7da0f9cdf433b9a2622ca8e26e4
nhmikami <nhmikami@gmail.com>
2025-03-12 18:54:30 -0300

    fixed init

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
+/*   By: naharumi <naharumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 16:35:42 by cayamash          #+#    #+#             */
-/*   Updated: 2025/03/11 12:51:49 by cayamash         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:49:28 by naharumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ t_lev	**init_lev(char **ev)
 	i = 0;
 	len = ft_arrlen(ev);
 	lev = (t_lev **)malloc(len * sizeof(t_lev));
-	while (ev[i] != NULL)
+	while (!ev[i])
 	{
 		arr_ev = ft_split(ev[i], '=');
 		node = ft_levnew(arr_ev);
